5af6f1fb2ffb53309a4b986c940b5a5419000deb
Hugo Kempfer <hugkempf@gmail.com>
2026-03-22 17:04:36 +0900

    Improve lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,7 +13,21 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = {
+          {
+            function()
+              local clients = vim.lsp.get_clients({ bufnr = 0 })
+              if #clients == 0 then return '' end
+              local names = {}
+              for _, client in ipairs(clients) do
+                table.insert(names, client.name)
+              end
+              return ' ' .. table.concat(names, ', ')
+            end,
+            cond = function() return #vim.lsp.get_clients({ bufnr = 0 }) > 0 end,
+          },
+          'filetype',
+        },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
